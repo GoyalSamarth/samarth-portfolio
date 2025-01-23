0e5acf3549e8f2673f4919ca64ce315a7ddfc41a
GoyalSamarth <gsamarth011@gmail.com>
2025-01-22 21:08:15 -0500

    update next config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Portfolio from "@/components/Portfolio";
-import Image from "next/image";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { motion } from "framer-motion";
